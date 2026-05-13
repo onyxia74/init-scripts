d0e12d04cda9613d80891ac38c7b958055754bad
onyxia74 <onyxia74@gmail.com>
2026-05-13 15:49:37 +0200

    Update Onyxia init script for vscode-python: init-scripts/vscode-python/exercice-addition.sh

--- init-scripts/vscode-python/exercice-addition.sh
+++ init-scripts/vscode-python/exercice-addition.sh
@@ -8,9 +8,9 @@ set -euo pipefail
 WORK_DIR="${WORK_DIR:-/home/onyxia/work}"
 mkdir -p "${WORK_DIR}"
 # Create workspace files.
-mkdir -p "$(dirname "${WORK_DIR}/addition.py")"
-cat > "${WORK_DIR}/addition.py" <<'ONYXIA_FILE_b4a2df3992d7'
+mkdir -p "$(dirname "${WORK_DIR}/definition.py")"
+cat > "${WORK_DIR}/definition.py" <<'ONYXIA_FILE_cd565a623781'
 def addition(a, b):
     return a + b
-ONYXIA_FILE_b4a2df3992d7
+ONYXIA_FILE_cd565a623781
 
