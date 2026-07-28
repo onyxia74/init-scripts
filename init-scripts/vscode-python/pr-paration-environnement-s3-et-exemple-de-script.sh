db81049d03df00ff5c921cad234a01a55490dbc7
onyxia74 <onyxia74@gmail.com>
2026-07-28 16:03:59 +0200

    Update Onyxia init script for vscode-python: init-scripts/vscode-python/pr-paration-environnement-s3-et-exemple-de-script.sh

--- init-scripts/vscode-python/pr-paration-environnement-s3-et-exemple-de-script.sh
+++ init-scripts/vscode-python/pr-paration-environnement-s3-et-exemple-de-script.sh
@@ -9,7 +9,7 @@ WORK_DIR="${WORK_DIR:-/home/onyxia/work}"
 mkdir -p "${WORK_DIR}"
 # Create workspace files.
 mkdir -p "$(dirname "${WORK_DIR}/s3_test.py")"
-cat > "${WORK_DIR}/s3_test.py" <<'ONYXIA_FILE_a769e9f752a4'
+cat > "${WORK_DIR}/s3_test.py" <<'ONYXIA_FILE_1bcbc6d78228'
 import boto3
 import os
 
@@ -21,16 +21,15 @@ session = boto3.Session(
     region_name=os.getenv('AWS_DEFAULT_REGION')
 )
 
-s3 = session.client('s3', endpoint_url=os.getenv('AWS_ENDPOINT'))
+s3 = session.client('s3',
+    endpoint_url=os.getenv('AWS_ENDPOINT_URL')
+)
 
-print('Connexion S3 réussie avec l\'endpoint:', os.getenv('AWS_ENDPOINT'))
+print('Connexion S3 réussie avec les secrets injectés.')
 # Liste les buckets pour tester
-try:
-    response = s3.list_buckets()
-    print('Buckets trouvés:', [b['Name'] for b in response['Buckets']])
-except Exception as e:
-    print('Erreur lors de la connexion:', e)
-ONYXIA_FILE_a769e9f752a4
+# response = s3.list_buckets()
+# print(response)
+ONYXIA_FILE_1bcbc6d78228
 
 # Install Python packages.
 PYTHON_BIN="${PYTHON_BIN:-python}"
