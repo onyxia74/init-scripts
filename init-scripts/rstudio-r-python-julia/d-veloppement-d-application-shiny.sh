c32b2c041bd74756d09f214f4167c66b4417c17c
onyxia74 <onyxia74@gmail.com>
2026-08-10 15:29:54 +0200

    Update Onyxia init script for rstudio-r-python-julia: init-scripts/rstudio-r-python-julia/d-veloppement-d-application-shiny.sh

--- init-scripts/rstudio-r-python-julia/d-veloppement-d-application-shiny.sh
+++ init-scripts/rstudio-r-python-julia/d-veloppement-d-application-shiny.sh
@@ -7,21 +7,6 @@ set -euo pipefail
 
 WORK_DIR="${WORK_DIR:-/home/onyxia/work}"
 mkdir -p "${WORK_DIR}"
-# Create workspace files.
-mkdir -p "$(dirname "${WORK_DIR}/app.R")"
-cat > "${WORK_DIR}/app.R" <<'ONYXIA_FILE_17cbee093bce'
-library(shiny)
-
-uui <- fluidPage(
-  "Hello Shiny!"
-)
-
-server <- function(input, output) {
-}
-
-shinyApp(ui = ui, server = server)
-ONYXIA_FILE_17cbee093bce
-
 # Install R packages.
-Rscript -e 'install.packages(c('"'"'shiny'"'"'), repos='"'"'https://cloud.r-project.org'"'"')'
+Rscript -e 'install.packages(c('"'"'shiny'"'"', '"'"'shinythemes'"'"', '"'"'shinydashboard'"'"'), repos='"'"'https://cloud.r-project.org'"'"')'
 
