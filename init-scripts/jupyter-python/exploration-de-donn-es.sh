afdebd5f1186399095731b0d2595eaca2fd1f117
onyxia74 <onyxia74@gmail.com>
2026-08-24 15:27:56 +0200

    Update Onyxia init script for jupyter-python: init-scripts/jupyter-python/exploration-de-donn-es.sh

--- init-scripts/jupyter-python/exploration-de-donn-es.sh
+++ init-scripts/jupyter-python/exploration-de-donn-es.sh
@@ -12,5 +12,5 @@ PYTHON_BIN="${PYTHON_BIN:-python}"
 if ! command -v "${PYTHON_BIN}" >/dev/null 2>&1; then
   PYTHON_BIN="python3"
 fi
-"${PYTHON_BIN}" -m pip install pandas matplotlib seaborn scipy scikit-learn plotly
+"${PYTHON_BIN}" -m pip install pandas matplotlib seaborn numpy scipy plotly
 
