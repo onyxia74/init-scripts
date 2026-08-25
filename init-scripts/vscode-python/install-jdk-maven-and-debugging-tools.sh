823ed4efbf29cc3d681992e60885c53e7b529b55
onyxia74 <onyxia74@gmail.com>
2026-08-25 11:37:40 +0200

    Update Onyxia init script for vscode-python: init-scripts/vscode-python/install-jdk-maven-and-debugging-tools.sh

--- init-scripts/vscode-python/install-jdk-maven-and-debugging-tools.sh
+++ init-scripts/vscode-python/install-jdk-maven-and-debugging-tools.sh
@@ -7,10 +7,5 @@ set -euo pipefail
 
 WORK_DIR="${WORK_DIR:-/home/onyxia/work}"
 mkdir -p "${WORK_DIR}"
-# Install Python packages.
-PYTHON_BIN="${PYTHON_BIN:-python}"
-if ! command -v "${PYTHON_BIN}" >/dev/null 2>&1; then
-  PYTHON_BIN="python3"
-fi
-"${PYTHON_BIN}" -m pip install openjdk-17-jdk maven
-
+sudo apt-get update
+sudo apt-get install -y default-jdk maven
