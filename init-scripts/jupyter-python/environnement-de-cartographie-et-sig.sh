f9eb338b43447728060b2854de8bd85690797448
onyxia74 <onyxia74@gmail.com>
2026-08-25 11:45:00 +0200

    Update Onyxia init script for jupyter-python: init-scripts/jupyter-python/environnement-de-cartographie-et-sig.sh

--- init-scripts/jupyter-python/environnement-de-cartographie-et-sig.sh
+++ init-scripts/jupyter-python/environnement-de-cartographie-et-sig.sh
@@ -12,5 +12,5 @@ PYTHON_BIN="${PYTHON_BIN:-python}"
 if ! command -v "${PYTHON_BIN}" >/dev/null 2>&1; then
   PYTHON_BIN="python3"
 fi
-"${PYTHON_BIN}" -m pip install geopandas folium shapely matplotlib contextily geoviews
+"${PYTHON_BIN}" -m pip install geopandas folium matplotlib shapely contextily rasterio
 
