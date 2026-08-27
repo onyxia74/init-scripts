0f4dd85936d84faa4589bbe6c601f966fa01d4bf
onyxia74 <onyxia74@gmail.com>
2026-08-27 22:23:29 +0200

    Add Onyxia init script for rstudio-r-python-julia: init-scripts/rstudio-r-python-julia/setup-environment-for-shiny-application-development.sh

---
+++ init-scripts/rstudio-r-python-julia/setup-environment-for-shiny-application-development.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# This init script prepares rstudio-r-python-julia on Onyxia.
+# Purpose: setup environment for Shiny application development
+# Expected parameters : None
+
+WORK_DIR="${WORK_DIR:-/home/onyxia/work}"
+mkdir -p "${WORK_DIR}"
+# Install R packages.
+Rscript -e 'install.packages(c('"'"'shiny'"'"', '"'"'shinythemes'"'"', '"'"'shinydashboard'"'"'), repos='"'"'https://cloud.r-project.org'"'"')'
+
