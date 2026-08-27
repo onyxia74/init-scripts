0af384df3e70eb91500cc672959cc6119fc4ef59
onyxia74 <onyxia74@gmail.com>
2026-08-28 00:22:06 +0200

    Update Onyxia init script for vscode-python: init-scripts/vscode-python/install-jdk-maven-and-debugging-tools.sh

--- init-scripts/vscode-python/install-jdk-maven-and-debugging-tools.sh
+++ init-scripts/vscode-python/install-jdk-maven-and-debugging-tools.sh
@@ -13,12 +13,8 @@ if [ "$(id -u)" -ne 0 ] && command -v sudo >/dev/null 2>&1; then
   APT_GET="sudo apt-get"
 fi
 ${APT_GET} update
-${APT_GET} install -y openjdk-17-jdk maven
+${APT_GET} install -y default-jdk maven
 
-# Install Python packages.
-PYTHON_BIN="${PYTHON_BIN:-python}"
-if ! command -v "${PYTHON_BIN}" >/dev/null 2>&1; then
-  PYTHON_BIN="python3"
-fi
-"${PYTHON_BIN}" -m pip install debugpy
+# Install VSCode/code-server extensions.
+code-server --install-extension vscjava.vscode-java-debug
 
