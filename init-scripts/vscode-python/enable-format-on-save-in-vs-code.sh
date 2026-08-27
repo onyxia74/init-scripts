b901f54f894d59762ba82db4eae4396a57890ece
onyxia74 <onyxia74@gmail.com>
2026-08-27 22:17:23 +0200

    Add Onyxia init script for vscode-python: init-scripts/vscode-python/enable-format-on-save-in-vs-code.sh

---
+++ init-scripts/vscode-python/enable-format-on-save-in-vs-code.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# This init script prepares vscode-python on Onyxia.
+# Purpose: Enable Format on Save in VS Code
+# Expected parameters : None
+
+WORK_DIR="${WORK_DIR:-/home/onyxia/work}"
+mkdir -p "${WORK_DIR}"
+mkdir -p "${WORK_DIR}/.vscode"
+
+# Create workspace files.
+mkdir -p "$(dirname "${WORK_DIR}/settings.json")"
+cat > "${WORK_DIR}/settings.json" <<'ONYXIA_FILE_b692fe4f5488'
+{"editor.formatOnSave": true}
+ONYXIA_FILE_b692fe4f5488
+mkdir -p "$(dirname "${WORK_DIR}/.vscode/settings.json")"
+cat > "${WORK_DIR}/.vscode/settings.json" <<'ONYXIA_FILE_0325085badab'
+{
+  "editor.formatOnSave": true
+}
+ONYXIA_FILE_0325085badab
+
