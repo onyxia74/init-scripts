3aaca7d71f912b0e0d2dd8643ae600df9b397e75
onyxia74 <onyxia74@gmail.com>
2026-08-27 12:44:21 +0200

    Add Onyxia init script for service: init-scripts/service/chart_id-vscode-python-packages-pandas-purpose-installation-d-extensions-vs-code-java-et-de-la-librairie-pandas-vscode_extensions-vscjava.vscode-java-pack.sh

---
+++ init-scripts/service/chart_id-vscode-python-packages-pandas-purpose-installation-d-extensions-vs-code-java-et-de-la-librairie-pandas-vscode_extensions-vscjava.vscode-java-pack.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# This init script prepares service on Onyxia.
+# Purpose: {chart_id:<|"|>vscode-python<|"|>,packages:[<|"|>pandas<|"|>],purpose:<|"|>installation d'extensions VS Code (Java) et de la librairie pandas<|"|>,vscode_extensions:[<|"|>vscjava.vscode-java-pack<|"|>]}
+# Expected parameters : None
+
+WORK_DIR="${WORK_DIR:-/home/onyxia/work}"
+mkdir -p "${WORK_DIR}"
+echo "No initialization action requested."
