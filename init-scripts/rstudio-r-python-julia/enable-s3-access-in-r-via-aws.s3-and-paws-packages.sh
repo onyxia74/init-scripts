b38b0956c0f24cc975df21100f96a3f20f1716e8
onyxia74 <onyxia74@gmail.com>
2026-08-28 10:24:18 +0200

    Add Onyxia init script for rstudio-r-python-julia: init-scripts/rstudio-r-python-julia/enable-s3-access-in-r-via-aws.s3-and-paws-packages.sh

---
+++ init-scripts/rstudio-r-python-julia/enable-s3-access-in-r-via-aws.s3-and-paws-packages.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# This init script prepares rstudio-r-python-julia on Onyxia.
+# Purpose: enable S3 access in R via aws.s3 and paws packages
+# Expected parameters : None
+
+WORK_DIR="${WORK_DIR:-/home/onyxia/work}"
+mkdir -p "${WORK_DIR}"
+# Install R packages.
+Rscript -e 'install.packages(c('"'"'aws.s3'"'"', '"'"'paws'"'"'), repos='"'"'https://cloud.r-project.org'"'"')'
+
